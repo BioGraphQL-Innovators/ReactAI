2cfa6dec72c04c9ad7f72ef45083e9c667a36e29
sanjay9 <sanjaymahabir9@gmail.com>
2023-04-16 15:39:38 -0400

    final

--- src/App.js
+++ src/App.js
@@ -8,48 +8,72 @@ import { Fragment } from "react";
 function App() {
   const passageRef = useRef(null);
   const questionRef = useRef(null);
-  const [answer, setAnswer] = useState();
+  const [answers, setAnswer] = useState();
   const [model, setModel] = useState(null);
   const loadModel = async () => {
-    const loadedModel = await qna.load();
-    setModel(loadedModel);
-    console.log('Model loaded')
+    try {
+      const loadedModel = await qna.load();
+      setModel(loadedModel);
+      //console.log('Model loaded')
+    }
+    catch (error) {
+      console.log(error);
+      //alert("An error occured");
+    }
   }
 
-  useEffect(()=>{loadModel()}, [])
+  useEffect(() => { loadModel() }, [])
 
-  const answerQuestion = async (e) =>{
-    console.log("AQ called");
-    if (e.which === 13 && model !== null ){
-    console.log('Question submitted.')
-    const passage = passageRef.current.value
-    const question = questionRef.current.value
-    const answers = await model.findAnswers(question,
-    passage)
-    setAnswer(answers);
-    console.log(answers)
+  const answerQuestion = async (e) => {
+    //console.log("AQ called");
+    if (e.which === 13 && model !== null) {
+      //console.log('Question submitted.')
+      const passage = passageRef.current.value
+      const question = questionRef.current.value
+      const answers = await model.findAnswers(question,
+        passage)
+      setAnswer(answers);
+      //console.log(answers)
     }
-    }
-    
-    
+  }
+
+
 
 
   return (
     <div className="App">
-    {model == null ?
-  <div>
-    <div>Model Loading</div>
-  </div> :
-  <Fragment>
-    passage
-    <textarea ref={passageRef} rows="30" cols="100"></textarea>
-    Ask a question
-    <input ref={questionRef} onKeyPress={answerQuestion} size="80"></input>
-    answers
-    {answer ? (answer.map((ans,idx)=><div><b>Answer{idx+1} = </b>{ans.text} {ans.score}</div>)):""}
-  </Fragment>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "center", background: "radial-gradient(#e90000, transparent)", marginBottom: "50px" }}>
+        <img src={logo} className="App-logo" alt="logo" style={{ width: "150px", height: "auto" }} />
+        <h1 style={{ fontWeight:"bold"}} >ReactAI</h1>
+      </div>
 
-  }
+      {model == null ?
+        <div style={{ width: "fit-content", margin: "auto" }}>
+          <button class="btn btn-primary" type="button" disabled style={{ fontSize: "40px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <span style={{ width: "3rem", height: "3rem" }} class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            &nbsp;Model is Loading...
+          </button>
+        </div> :
+        <div style={{ width: "70%", margin:"auto" }}>
+        
+          <div class="mb-3" >
+            <label  for="exampleFormControlTextarea1" class="form-label">Passage</label>
+            <textarea ref={passageRef} class="form-control" id="exampleFormControlTextarea1" rows="10" ></textarea>
+          </div>
+
+
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Ask a question</label>
+            <input ref={questionRef} onKeyPress={answerQuestion} class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+          </div>
+
+
+         
+          <label>Answers</label>
+          {answers ? (answers.map((ans, idx) => <div id="ans"><b>Answer{idx + 1} = </b>{ans.text} {ans.score}</div>)) : ""}
+        </div>
+
+      }
     </div>
   );
 }
